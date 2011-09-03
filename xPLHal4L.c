44c564069760a974a9172a0493f14c57cd8e7e38
cpoyet (foxg20) <cpoyet@yahoo.fr>
2011-09-03 13:16:47 +0200

    Ajout des option ligne de commande

--- xPLHal4L.c
+++ xPLHal4L.c
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <ctype.h>
+#include <getopt.h>
 
 #include <time.h>
 #include <signal.h>
@@ -18,10 +19,46 @@
 #define XPLHAL4L_VERSION "1.0"
 
 
+int getOptions( int argc, char **argv)
+{
+	int c;
+	int option_index = 0;
+	struct option long_options[] =
+	{
+		{"config", 	1, NULL, 'c'},
+		{"help", 	0, NULL, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	while (1)
+	{
+		c = getopt_long (argc, argv, "c:h", long_options, &option_index);
+		if (c == -1)
+			break;
+			
+		switch (c)
+		{
+            case 'c':
+				printf ("option c with value `%s'\n", optarg);
+				break;
+			case 'h':
+				printf ("option help\n");
+				break;
+		}
+
+	}
+	
+	return 0;
+}
+
 int main (int argc, String argv[])
 {
     /* Parse command line parms */
-    if (!xPL_parseCommonArgs (&argc, argv, FALSE)) exit (1);
+    if (!xPL_parseCommonArgs (&argc, argv, FALSE))
+    {
+        fprintf (stderr, "Unable to parse xPL args");
+        exit (1);
+    }
     
     /* Start xPL up */
     if (!xPL_initialize (xPL_getParsedConnectionType ()))
@@ -29,9 +66,13 @@ int main (int argc, String argv[])
         fprintf (stderr, "Unable to start xPL");
         exit (1);
     }
-
 	
-	//XHCP_printMessage(0, RES_HALWELCOM);
+	if (!getOptions(argc, argv))
+    {
+        fprintf (stderr, "Error parsing common args");
+        exit (1);
+    }
+	
 	 XHCP_server(0, NULL);
 
 }

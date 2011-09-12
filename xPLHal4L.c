0130b70302277d7b60846e50532db11cea9bc30a
cpoyet <cpoyet@yahoo.fr>
2011-09-12 17:24:47 +0300

    Chargement du fichier config.xml par defaut

--- xPLHal4L.c
+++ xPLHal4L.c
@@ -3,58 +3,58 @@
 
 #include "xPLHal4L.h"
 
-int getOptions( int argc, char **argv)
+int getOptions ( int argc, char **argv)
 {
-	int c;
-	int option_index = 0;
-	struct option long_options[] =
-	{
-		{"config", 		1, NULL, 'c'},
-		{"xhcpconfig", 	1, NULL, 'x'},
-		{"help", 		0, NULL, 'h'},
-		{0, 0, 0, 0}
-	};
-
-	while (1)
-	{
-		c = getopt_long (argc, argv, "x:c:h", long_options, &option_index);
-		if (c == -1)
-			break;
-			
-		switch (c)
-		{
+    int c;
+    int option_index = 0;
+    struct option long_options[] =
+    {
+        {"config", 		1, NULL, 'c'},
+        {"xhcpconfig", 	1, NULL, 'x'},
+        {"help", 		0, NULL, 'h'},
+        {0, 0, 0, 0}
+    };
+    
+    while (1)
+    {
+        c = getopt_long (argc, argv, "x:c:h", long_options, &option_index);
+        if (c == -1)
+            break;
+        
+        switch (c)
+        {
             case 'c':
-				HAL4L_setConfigFile(optarg);
-				break;
+                HAL4L_setConfigFile (optarg);
+                break;
             case 'x':
-				XHCP_setConfigFile(optarg);
-				break;
-			case 'h':
-				printf ("option help\n");
-				printf ("to do ...n");
-				exit(0);
-				break;
-		}
-
-	}
-	
-	return 1;
+                XHCP_setConfigFile (optarg);
+                break;
+            case 'h':
+                printf ("option help\n");
+                printf ("to do ...n");
+                exit (0);
+                break;
+        }
+        
+    }
+    
+    return 1;
 }
 
 
 
-int loadHal4lConfig(char *fileName)
+int loadHal4lConfig (char *fileName)
 {
-	printf("Loading xPLHal4L server configuration...\n");
-
-	if ( fileName == NULL )
-		Error_Quit("Unable to load xPLHal4L config file");
-
-	if ( (rootConfig = roxml_load_doc(fileName)) == NULL )
-		Error_Quit("Error loading xPLHal4L config file");
-	
-	return 0;
-
+    printf ("Loading xPLHal4L server configuration...\n");
+    
+    if ( fileName == NULL )
+        Error_Quit ("Unable to load xPLHal4L config file");
+    
+    if ( (rootConfig = roxml_load_doc (fileName)) == NULL )
+        Error_Quit ("Error loading xPLHal4L config file");
+    
+    return 0;
+    
 }
 
 int main (int argc, String argv[])
@@ -72,17 +72,22 @@ int main (int argc, String argv[])
         fprintf (stderr, "Unable to start xPL");
         exit (1);
     }
-	
-	if (!getOptions(argc, argv))
+    
+    if (!getOptions (argc, argv))
     {
         fprintf (stderr, "Error parsing common args");
         exit (1);
     }
-	
-	if ( HAL4L_getConfigFile() )
-		loadHal4lConfig(HAL4L_getConfigFile());
-	
-	
-	 XHCP_server(rootConfig);
-
+    
+    if ( HAL4L_getConfigFile ()==NULL )
+    {
+        printf ("Loading default configuration file: config.xml\n");
+        HAL4L_setConfigFile ("config.xml");
+    }
+    
+    loadHal4lConfig (HAL4L_getConfigFile ());
+    
+    
+    XHCP_server (rootConfig);
+    
 }

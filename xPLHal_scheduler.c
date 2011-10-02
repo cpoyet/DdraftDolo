40e55cdd70d07127cc290bea684c46020d353707
cpoyet (foxg20) <cpoyet@yahoo.fr>
2011-10-03 00:28:19 +0200

    comparaison internal tick et determinators time condittion

--- xPLHal_scheduler.c
+++ xPLHal_scheduler.c
@@ -1,8 +1,11 @@
 
 #define _XPLHAL_SCHEDULER_C_
 
+#define _XOPEN_SOURCE 
+
 #include "xPLHal_scheduler.h"
 #include "xPLHal4L.h"
+
 
 
 xPL_ServicePtr clockService = NULL;
@@ -10,29 +13,177 @@ xPL_ServicePtr schedulerService = NULL;
 xPL_MessagePtr clockTickMessage = NULL;
 xPL_MessagePtr schedulerTickMessage = NULL;
 
+int timeStr2int (char *str)
+{
+/*	int i = 0;
+	int h = 0, m = 0, *t;
+	unsigned int c = 0;
+	
+	t=&h;
+	for (i=0; i<strlen(str); i++)
+	{
+		c = str[i]-0x30;
+		if ( c > 9 )
+			t=&m;
+		else
+			*t = *t * 10 + c;
+	}
+	return h*60+m;*/
+	return (str[0]-0x30)*600+(str[1]-0x30)*60+(str[3]-0x30)*10+(str[4]-0x30);
+}
 
+int dateStr2int( char *str)
+{
+	struct tm ts;
+	char *r;
+
+	r = strptime(str, "%d %b %Y", &ts);
+	if ( r == NULL || *r !='\0')
+		r = strptime(str, "%Y %m %d", &ts);
+	if ( r == NULL || *r !='\0')
+		r = strptime(str, "d %B %Y", &ts);
+	if ( r == NULL || *r !='\0')
+		r = strptime(str, "b %d %Y", &ts);
+	if ( r == NULL || *r !='\0')
+		r = strptime(str, "b %B %Y", &ts);
+	 
+	
+
+	return ts.tm_year*10000+ts.tm_mon*100+ts.tm_mday;
+}
+
+int monthStr2int ( char *str)
+{
+	int value=0;
+	
+	if ( strncasecmp(str,"ja",2) == 0 )
+		value=1;
+	else if ( strncasecmp(str,"fe",2) == 0 )
+		value=2;
+	else if ( strncasecmp(str,"mar",3) == 0 )
+		value=3;
+	else if ( strncasecmp(str,"ap",2) == 0  || strncasecmp(str,"av",2) == 0 )
+		value=4;
+	else if ( strncasecmp(str,"may",3) == 0 || strncasecmp(str,"mai",3) == 0 )
+		value=5;
+	else if ( strncasecmp(str,"jun",3) == 0 || strncasecmp(str,"juin",4) == 0 )
+		value=6;
+	else if ( strncasecmp(str,"jul",3) == 0 || strncasecmp(str,"juil",4) == 0 )
+		value=7;
+	else if ( strncasecmp(str,"au",2) == 0 || strncasecmp(str,"ao",2) == 0 )
+		value=8;
+	else if ( strncasecmp(str,"se",2) == 0 )
+		value=9;
+	else if ( strncasecmp(str,"oc",2) == 0 )
+		value=10;
+	else if ( strncasecmp(str,"no",2) == 0 )
+		value=11;
+	else if ( strncasecmp(str,"de",2) == 0 )
+		value=12;
+
+	if ( value == 0 )
+		value = atoi(str);
+		
+	return value;
+	
+}
 void internalMessageHandler(xPL_MessagePtr theMessage, xPL_ObjectPtr userValue)
 {
-    node_t **result;
-    int nb_result;
+    node_t **tCondLst;
+    int nbCondLst;
+	char ct[10], op[32], va[80];
+	int sz_ct, sz_op, sz_va;
 
+	char *sourceVendor, *sourceDeviceID, *sourceInstanceID, *schemaClass, *schemaType;
+	xPL_MessageType messgeType;	
+	
+	char *timeStr;
+	int tickMonth, tickDate, tickDay, tickYear, tickTime;
+	int tickValue = 0;
 
-	char *sourceVendor = xPL_getSourceVendor (theMessage);
-	char *sourceDeviceID = xPL_getSourceDeviceID (theMessage);
-	char *sourceInstanceID = xPL_getSourceInstanceID (theMessage);
-	xPL_MessageType messgeType = xPL_getMessageType (theMessage);
-	char *schemaClass = xPL_getSchemaClass (theMessage);
-	char *schemaType = xPL_getSchemaType (theMessage);
+	int i;
+	char *tmp;	
+	
+	sourceVendor = xPL_getSourceVendor (theMessage);
+	sourceDeviceID = xPL_getSourceDeviceID (theMessage);
+	sourceInstanceID = xPL_getSourceInstanceID (theMessage);
+	messgeType = xPL_getMessageType (theMessage);
+	schemaClass = xPL_getSchemaClass (theMessage);
+	schemaType = xPL_getSchemaType (theMessage);
 
 	printf ( "Received a Message from %s-%s.%s of type %d for %s.%s\n",
 										sourceVendor, sourceDeviceID, sourceInstanceID, messgeType, schemaClass, schemaType);
+	/*
+	if ((tmp = xPL_formatMessage(theMessage)) == NULL)
+	{
+		printf("Erreur formattage message\n");
+		return;
+	}
+	printf("Message traité:\n%s\n", tmp);*/
+	
 	if ( strcmp(schemaClass, "internal") == 0)
 	{
 		if ( strcmp(schemaType, "tick") == 0 )
 		{
+			timeStr = xPL_getMessageNamedValue(theMessage, "time");
+/*			int timeMinutes = timeStr2int( timeStr );*/
+			tickMonth = atoi (xPL_getMessageNamedValue(theMessage, "month"));
+			tickDate  = atoi (xPL_getMessageNamedValue(theMessage, "date"));
+			tickDay   = atoi (xPL_getMessageNamedValue(theMessage, "day"));
+			tickYear  = atoi (xPL_getMessageNamedValue(theMessage, "year"));
+			tickTime  = timeStr2int( xPL_getMessageNamedValue(theMessage, "time") );
+			
+			tickValue = 0;
+			
+			
+			printf("heure %s => %d\n", timeStr, tickTime);
+			printf("tickMonth=%d tickDate=%d tickDay=%d tickYear=%d tickTime=%d\n", tickMonth, tickDate, tickDay, tickYear, tickTime );
+
 			/* On recherche tous les determinators contenant des conditions de temps */
-			result = roxml_xpath ( rootConfig, "//determinator//timeCondition", &nb_result);
-			printf("%d timeConditions trouvées\n",nb_result);
+			tCondLst = roxml_xpath ( rootConfig, "//determinator//timeCondition", &nbCondLst);
+			printf("%d timeConditions trouvées\n",nbCondLst);
+			
+			
+			for ( i=0; i<nbCondLst; i++)
+			{
+				//char *ct=roxml_get_attr (tCondLst[i], "category", 0);
+				roxml_get_content ( roxml_get_attr (tCondLst[i], "category", 0), ct, 80, &sz_ct );
+				//char *op=roxml_get_attr (tCondLst[i], "operator", 0);
+				roxml_get_content ( roxml_get_attr (tCondLst[i], "operator", 0), op, 80, &sz_op );
+				//char *va=roxml_get_attr (tCondLst[i], "value", 0);
+				roxml_get_content ( roxml_get_attr (tCondLst[i], "value", 0), va, 80, &sz_va );
+				
+				int value;
+				
+				if (strcasecmp(ct,"time") == 0 )
+				{
+					tickValue = tickTime;
+					value = timeStr2int(va);
+				}
+				else if (strcasecmp(ct,"date") == 0 )
+				{
+					tickValue = tickDate;
+					value = dateStr2int(va);
+				}
+				else if (strcasecmp(ct,"day") == 0 )
+				{
+					tickValue = tickDay;
+					value = atoi(va);
+				}
+				else if (strcasecmp(ct,"month") == 0 )
+				{
+					tickValue = tickMonth;
+					value = monthStr2int(va);
+				}
+				else if (strcasecmp(ct,"year") == 0 )
+				{
+					tickValue = tickYear;
+					value = atoi(va);
+				}
+				
+				printf("ct=%s va=%s tickvalue=%d op=%s value=%d\n",ct, va, tickValue, op, value); 
+				
+			}
 			
 		}
 	}
@@ -234,8 +385,19 @@ int xpl4l_timer(node_t* argXmlConfig)
 		strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S %Z", ts);
 		
 		/* Crée le messageBody si NULL et y ajoute une seule ligne si elle n'existe pas, sinon la met à jour */
+		xPL_setMessageNamedValue(schedulerTickMessage, "timestamp", buf);
+		strftime(buf, sizeof(buf), "%H:%M", ts);
 		xPL_setMessageNamedValue(schedulerTickMessage, "time", buf);
+		strftime(buf, sizeof(buf), "%Y", ts);
+		xPL_setMessageNamedValue(schedulerTickMessage, "year", buf);
+		strftime(buf, sizeof(buf), "%m", ts);
+		xPL_setMessageNamedValue(schedulerTickMessage, "month", buf);
+		strftime(buf, sizeof(buf), "%Y%m%d", ts);
+		xPL_setMessageNamedValue(schedulerTickMessage, "date", buf);
+		strftime(buf, sizeof(buf), "%d", ts);
+		xPL_setMessageNamedValue(schedulerTickMessage, "day", buf);
 
+		
 		ret = xPL_dispatchMessageEvent(schedulerTickMessage);
 
 		printf ("Message dispatched = %s\n", ret == TRUE ? "TRUE":"FALSE");
